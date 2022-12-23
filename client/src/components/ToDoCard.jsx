7b4b90ef8dbdab52ca0514d3666928575a8b58f6
John Furlong <johnfurlong24@gmail.com>
2022-12-23 11:10:39 -0700

    add expanding responsive card inputs

--- client/src/components/ToDoCard.jsx
+++ client/src/components/ToDoCard.jsx
@@ -1,25 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from '../../styles/ToDoCard.module.scss'
 import Icon from '../components/Icon.jsx'
 import { ICONS } from '../components/Icon.jsx'
 import { AiOutlinePlusCircle } from 'react-icons/ai'
 
 const ToDoCard = ({ habit }) => {
-  return (
-    <div className={styles.card} style={{ backgroundColor: habit.color }}>
-        <span className={styles.iconContainer} style={{ margin: '1rem' }}>
-        {!!habit?.icon
-            ? ICONS[habit.icon].icon
-            : null
-        }
-        </span>
-        <h1 className={styles.title}>{habit.title}</h1>
-        <AiOutlinePlusCircle
-            className={styles.addBtn}
-            onClick={() => {}}
-        />
-    </div>
-  )
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        // <div className={styles.card} style={{ backgroundColor: habit.color }}>
+        //     <h1 className={styles.title}>{habit.title}</h1>
+        // </div>
+
+        <div
+        className={styles["container"] + " " + (isOpen ? styles["expand"] : "")}
+        style={{ backgroundColor: habit.color }}
+      >
+        <div className={styles["upper"]}>
+            <span className={styles.iconContainer} style={{ margin: '1rem' }}>
+            {!!habit?.icon || habit?.icon === 0
+                ? ICONS[habit.icon].icon
+                : null
+            }
+            </span>
+            <h1 className={styles.title}>{habit.title}</h1>
+            <span 
+                class={styles["close-btn"] + ' ' + (isOpen ? styles['open'] : '')}
+                onClick={() => setIsOpen(!isOpen)}
+            />
+        </div>
+        <div className={styles["lower"]}>
+          <h3>Goal: {habit.frequency} {habit.units}</h3>
+          <div>Every: {habit.schedule.split(',').map((day, idx) => 
+            <p>{day}</p>
+          )} {habit.units}</div>
+          <h3>
+            {habit.description}
+          </h3>
+        </div>
+      </div>
+    )
 }
 
 export default ToDoCard
